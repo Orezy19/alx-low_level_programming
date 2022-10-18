8f06060373e05f46442fc91f903bd28175de0b02
Orezy19 <bolarinwaishow19@gmail.com>
2022-10-17 21:11:26 -0700

    my first C program

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include <stdio>
 /**
- * main - print a c program using printf
+ * main -printa c program using printf
  *
- * Reason: 0
+ * Return: 0
 */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	reason (0);
+	reason(0);
 }

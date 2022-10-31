55072107066cc7f032412e4cc645f6c0506d94dd
Orezy19 <bolarinwaishow19@gmail.com>
2022-10-30 17:07:29 -0700

    Ore

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * puts_half - Prints a string
+ * @str: parameter string
+ *
+ * Return: Function is void
+ */
+void puts_half(char *str)
+{
+	int i, count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			break;
+		}
+		count++;
+	}
+	if (count % 2 == 1)
+	{
+		i = count / 2;
+	}
+	for (i++; i < count; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}

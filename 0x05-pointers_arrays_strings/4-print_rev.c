f1d2db16b2fb7e133e1b9808abf82cce01600893
Orezy19 <bolarinwaishow19@gmail.com>
2022-10-30 17:17:31 -0700

    Pre

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,24 +3,15 @@
  * print_rev - Prints reverse string
  * @s: parameter variable
  *
- * Return: A void function
  */
-
 void print_rev(char *s)
 {
-	int count = 0;
+	int i = 0;
 
-	while (count >= 0)
-	{
-		if (s[count] == '\0')
-		{
-			break;
-		}
-		count++;
-	}
-	for (count--; count >= 0; count--)
-	{
-		_putchar(s[count]);
-	}
+	while (s[i] != '\0')
+		i++;
+	for (i = i - 1; i >= 0; i--)
+		_putchar(s[i]);
+
 	_putchar('\n');
 }

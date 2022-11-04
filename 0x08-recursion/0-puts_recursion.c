140861c2b1e1202ba9a0d1151751bd97f17fca55
Orezy19 <bolarinwaishow19@gmail.com>
2022-11-03 21:42:07 -0700

    Ore

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - This function prints a string
+ * @s: string parameter
+ */
+
+void _puts_recursion(char *s)
+{
+	/* Checks if the string parameter is empty */
+	if (*s == '\0')
+	{
+		/* prints out a new line */
+		_putchar('\n');
+		return;		/* breaks out of the function*/
+	}
+	_putchar(*s);	/* prints first character */
+	s++;			/* increment the address of the first element */
+	_puts_recursion(s);	/* recall the function */
+}

567dabdfbb30854daa23a860376bc828313a6794
Orezy19 <bolarinwaishow19@gmail.com>
2022-11-03 21:58:24 -0700

    m

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - This function prints a string in reverse
+ * @s: string parameter
+ */
+
+void _print_rev_recursion(char *s)
+{
+	/* check if the string is null */
+	if (*s == '\0')
+	{
+		return;
+	}
+	s++;	/* increment the address of the first indes */
+	_print_rev_recursion(s);	/* recall the funtion till it is null */
+	s--;	/* when function is null, the index decrements by 1 */
+	_putchar(*s);   /* prints the value of *s */
+}

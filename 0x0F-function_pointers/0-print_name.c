95ec2ed58ee08e3f9af391d15d2843f47f79442a
Orezy19 <bolarinwaishow19@gmail.com>
2022-11-16 11:03:11 -0800

    0x0F

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function prints a name
+ * @name: string parameter
+ * @f: another parameter
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
